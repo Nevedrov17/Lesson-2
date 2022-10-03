610c775cb4fd99fe6c3e100cbc52c49e238741df
NEVA <neva.vka@yandex.ru>
2022-10-03 21:00:55 +0300

    Массив2

---
+++ Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {11, 21, 17, 13, 4, 511, 6 ,17, 18};
+
+int n = array.Length;
+int find = 17;
+
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
